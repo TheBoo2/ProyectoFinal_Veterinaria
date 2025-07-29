48e1f186204c8b4f883226f99225dd4c04c014be
TheBoo2 <81713486+TheBoo2@users.noreply.github.com>
2025-07-29 00:58:31 -0500

    Delete PawPlus_Veterinaria archivos antiguos

--- PawPlus_Veterinaria/Chat.java
+++
@@ -1,7 +0,0 @@
-package PawPlus_Veterinaria;
-
-import javax.swing.*;
-
-public class Chat {
-    private JButton button1;
-}
--- PawPlus_Veterinaria/Main.java
+++
@@ -1,8 +0,0 @@
-package PawPlus_Veterinaria;
-
-public class Main{
-    public static void main(String[] args) {
-        PawPlus bot = new PawPlus();
-        bot.iniciarConversacion();
-    }
-}
